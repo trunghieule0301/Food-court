d66754612c22bfb526d1330b3fb8c080c78bb8e4
DangNguyen <nhockbayoka@gmail.com>
2020-06-19 22:56:44 +0700

    Frame 4 done

---
+++ app/src/main/java/com/example/myapplication/Authenticate.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Authenticate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authenticate);
+    }
+}
--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -9,16 +9,24 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     Button btnStart;
+    Button btnMe1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnMe1 = (Button) findViewById(R.id.button5);
         btnStart = (Button) findViewById(R.id.button8);
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, Foodstall.class);
                 startActivity(intent);
+            }
+        });
+        btnMe1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
             }
         });
     }
