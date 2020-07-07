4e82d12c9b131e7a459e8260d337514dd6f10674
trunghieule0301 <trunghieule0301@gmail.com>
2020-07-07 16:55:24 +0700

    new updates for fragments

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,26 +3,21 @@ package com.example.myapplication;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.example.myapplication.ui.account.AccountFragment;
 import com.example.myapplication.ui.foodstall.FoodstallFragment;
 import com.example.myapplication.ui.home.HomeFragment;
-import com.example.myapplication.ui.promotion.PromotionFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.view.WindowManager;
 import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE); // for hiding title
@@ -32,29 +27,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+
     }
 
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
-            new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                        Fragment selectedFragment = null;
-                        switch (menuItem.getItemId()){
-                            case R.id.navigation_home:
-                                selectedFragment = new HomeFragment();
-                                break;
-                            case R.id.navigation_foodstall:
-                                selectedFragment = new FoodstallFragment();
-                                break;
-                            case R.id.navigation_account:
-                                selectedFragment = new AccountFragment();
-                                break;
-                            case R.id.navigation_promotion:
-                                selectedFragment = new PromotionFragment();
-                                break;
-                        }
-                        getSupportFragmentManager().beginTransaction().replace(R.id.container, selectedFragment).commit();
-                        return true;
-                    }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+            switch (menuItem.getItemId()){
+                case R.id.navigation_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.navigation_foodstall:
+                    selectedFragment = new FoodstallFragment();
+                    break;
+                case R.id.navigation_account:
+                    selectedFragment = new AccountFragment();
+                    break;
+            }
+            getSupportFragmentManager().beginTransaction().replace(R.id.container, selectedFragment).commit();
+            return true;
+        }
     };
 }
--- app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/ui/home/HomeFragment.java
@@ -1,5 +1,6 @@
 package com.example.myapplication.ui.home;
 
+import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
 import android.view.View;
@@ -12,12 +13,23 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.denzcoskun.imageslider.ImageSlider;
+import com.denzcoskun.imageslider.constants.ScaleTypes;
+import com.denzcoskun.imageslider.models.SlideModel;
 import com.example.myapplication.R;
+
+import java.lang.String;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
+    Context thiscontext;
+
     private HomeViewModel homeViewModel;
 
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel = ViewModelProviders.of(this).get(HomeViewModel.class);
@@ -29,6 +41,24 @@ public class HomeFragment extends Fragment {
                 textView.setText(s);
             }
         });
+
+        thiscontext = container.getContext();
+
         return root;
     }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ImageSlider imageSlider = view.findViewById(R.id.image_slider);
+        List<SlideModel> slideModels = new ArrayList<>();
+        slideModels.add(new SlideModel(R.drawable.food1, ScaleTypes.FIT)); // for one image
+        slideModels.add(new SlideModel(R.drawable.food2, ScaleTypes.FIT)); // for one image
+        slideModels.add(new SlideModel(R.drawable.food3, ScaleTypes.FIT)); // for one image
+        slideModels.add(new SlideModel(R.drawable.food4, ScaleTypes.FIT)); // for one image
+        slideModels.add(new SlideModel(R.drawable.food5, ScaleTypes.FIT)); // for one image
+        imageSlider.setImageList(slideModels, ScaleTypes.FIT); // for all images
+
+    }
 }
+
