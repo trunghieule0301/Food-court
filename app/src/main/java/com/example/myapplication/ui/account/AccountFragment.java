88459f71b11838970e695fd4d57fa21f7f79ed5e
dangnguyen1004 <nguyendangit04@gmail.com>
2020-07-15 15:26:33 +0700

    Account information in AccountFragment

--- app/src/main/java/com/example/myapplication/ui/account/AccountFragment.java
+++ app/src/main/java/com/example/myapplication/ui/account/AccountFragment.java
@@ -8,6 +8,7 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TableLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -21,120 +22,103 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonArrayRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.myapplication.Customer;
+import com.example.myapplication.Login;
 import com.example.myapplication.MainActivity;
 import com.example.myapplication.R;
 import com.example.myapplication.RegisterActivity;
+import com.google.android.material.button.MaterialButtonToggleGroup;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
+import java.sql.BatchUpdateException;
 import java.util.ArrayList;
 
 public class AccountFragment extends Fragment {
 
-//    Button buttonLogin, buttonRegister;
-//    EditText editTextAccount, editTextPassword;
-//    ArrayList<Customer> arrayListCustomer;
-//    private String url = "http://foodcourt2020.medianewsonline.com/getCusData.php";
+    Button buttonChangePasswordAccount, buttonLogoutAccount;
+    TextView textViewNameAcocunt, textViewAccount, textViewEmailAccount, textViewAgeAccount, textViewGenderAccount;
+    private String urlGetCusData = "http://foodcourt2020.medianewsonline.com/getCusData.php";
+    private ArrayList<Customer> arrayListCustomer;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_account, container, false);
-
-//        buttonLogin = (Button) root.findViewById(R.id.btnLogin);
-//
-//        buttonRegister = (Button) root.findViewById(R.id.btnRegister);
-//
-//        editTextAccount = (EditText) root.findViewById(R.id.edtAccount);
-//
-//        editTextPassword = (EditText) root.findViewById(R.id.edtPassword);
         return root;
     }
-//    public boolean CheckAccount(String account, String password){
-//        for (int i = 0; i < arrayListCustomer.size(); i++){
-//            if (arrayListCustomer.get(i).getAccount().equals(account) && arrayListCustomer.get(i).getPassword().equals(password)) {
-//                return true;
-//            }
-//        }
-//        return false;
-//    }
-//    public void GetCustomerData(String url, ArrayList<Customer> arrayListCustomer){
-//        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
-//        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
-//                new Response.Listener<JSONArray>() {
-//                    @Override
-//                    public void onResponse(JSONArray response) {
-//                        Toast.makeText(getActivity(), "Load Customer Data success", Toast.LENGTH_SHORT).show();
-//                        for (int i = 0;i < response.length(); i++){
-//                            try {
-//                                JSONObject object = response.getJSONObject(i);
-//                                arrayListCustomer.add(new Customer(
-//                                        object.getInt("ID"),
-//                                        object.getString("Account"),
-//                                        object.getString("Name"),
-//                                        object.getString("Email"),
-//                                        object.getString("PassWord"),
-//                                        object.getInt("Age"),
-//                                        object.getString("Sex"),
-//                                        object.getString("Address")));
-//                            } catch (JSONException e) {
-//                                e.printStackTrace();
-//                            }
-//                        }
-//                    }
-//                },
-//                new Response.ErrorListener() {
-//                    @Override
-//                    public void onErrorResponse(VolleyError error) {
-//                        Toast.makeText(getActivity(), "Load data fail due to " + error.toString(), Toast.LENGTH_SHORT).show();
-//
-//                    }
-//                });
-//        requestQueue.add(jsonArrayRequest);
-//    }
-//
-//    @Override
-//    public void onViewCreated(View view, Bundle savedInstanceState){
-//        super.onViewCreated(view, savedInstanceState);
-//        loadLoginActivity();
-//    }
-//
-//    @Override
-//    public void onResume() {
-//        super.onResume();
-//        loadLoginActivity();
-//    }
-//
-//    private void loadLoginActivity(){
-//        arrayListCustomer = new ArrayList<>();
-//        GetCustomerData(url, arrayListCustomer);
-//
-//        buttonLogin.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-////                Toast.makeText(getActivity(), editTextAccount.getText().toString(), Toast.LENGTH_SHORT).show();
-//                if (editTextAccount.getText().toString().equals("") || editTextPassword.getText().toString().equals("")){
-//                    Toast.makeText(getActivity(), "Please fill account and password", Toast.LENGTH_SHORT).show();
-//                }
-//                else{
-//                    if (CheckAccount(editTextAccount.getText().toString(),editTextPassword.getText().toString())){
-//                        Toast.makeText(getActivity(), "Login success", Toast.LENGTH_SHORT).show();
-//                    }
-//                    else{
-//                        Toast.makeText(getActivity(), "Login fail", Toast.LENGTH_SHORT).show();
-//                    }
-//                }
-//            }
-//        });
-//
-//        buttonRegister.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Toast.makeText(getActivity(), "Register", Toast.LENGTH_SHORT).show();
-//                Intent intent = new Intent(getActivity(), RegisterActivity.class);
-//                startActivity(intent);
-//
-//            }
-//        });
-//    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view, savedInstanceState);
+
+        Anhxa();
+        arrayListCustomer =  new ArrayList<>();
+        GetCustomerData(urlGetCusData, arrayListCustomer);
+        buttonChangePasswordAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+        buttonLogoutAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getActivity(), Login.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    private void Anhxa(){
+        buttonChangePasswordAccount = (Button) getView().findViewById(R.id.btnChangePasswordAccount);
+        buttonLogoutAccount = (Button) getView().findViewById(R.id.btnLogoutAccount);
+        textViewNameAcocunt = (TextView) getView().findViewById(R.id.textViewName);
+        textViewAccount = (TextView) getView().findViewById(R.id.textViewAccount);
+        textViewEmailAccount = (TextView) getView().findViewById(R.id.textViewEmail);
+        textViewAgeAccount = (TextView) getView().findViewById(R.id.textViewAge);
+        textViewGenderAccount = (TextView) getView().findViewById(R.id.textViewGender);
+    }
+
+    public void GetCustomerData(String url, ArrayList<Customer> arrayListCustomer){
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Toast.makeText(getActivity(), "Load Customer Data success", Toast.LENGTH_SHORT).show();
+                        for (int i = 0;i < response.length(); i++){
+                            try {
+                                JSONObject object = response.getJSONObject(i);
+                                arrayListCustomer.add(new Customer(
+                                        object.getInt("ID"),
+                                        object.getString("Account"),
+                                        object.getString("Name"),
+                                        object.getString("Email"),
+                                        object.getString("PassWord"),
+                                        object.getInt("Age"),
+                                        object.getString("Sex"),
+                                        object.getString("Address")));
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(getActivity(), "Load data fail due to " + error.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+        requestQueue.add(jsonArrayRequest);
+    }
+
 }
